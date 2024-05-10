fb2b5bf2bd969f2c578ba3e1195887c6a385c444
Gina Sciacchitano <gbell76767@gmail.com>
2024-05-10 13:46:53 -0400

    readme and comments

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-  // if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 }
